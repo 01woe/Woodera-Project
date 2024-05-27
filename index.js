de103b5c0ed1bf00a521ed5463eca8149b1efa45
01woe <150074762+01woe@users.noreply.github.com>
2024-05-27 12:28:04 +0300

    Project done

---
+++ index.js
@@ -0,0 +1,8 @@
+// Selectăm butonul "Get Started"
+const startButton = document.getElementById("startButton");
+
+// Adăugăm un eveniment de clic
+startButton.addEventListener("click", function () {
+  // Afisăm un mesaj de salut
+  alert("Welcome to Woodera! Bringing wood to your homes!");
+});
